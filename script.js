9533d1fc48bc9eb9c843b16275a498d38a065721
sumitsadafule <sumitsadafule0303@gmail.com>
2024-11-19 13:24:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu-button');
+    const navLinks = document.getElementById('nav-links');
+
+    menuButton.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
